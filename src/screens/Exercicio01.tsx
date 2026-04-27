cdcfe309fee444a27467bdf715228054246a0713
Francisco Paulino de Queiroz <franciscopaulino.q@gmail.com>
2026-04-27 15:57:44 -0300

    feat: implement exercicio 1 screen

--- src/screens/Exercicio01.tsx
+++ src/screens/Exercicio01.tsx
@@ -4,7 +4,32 @@ import { View, Text, StyleSheet } from "react-native";
 export default function Exercicio01() {
   return (
     <View style={styles.container}>
-      <Text style={styles.indicatorText}>Exercício 01 - DDM</Text>
+      <View style={styles.header}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>Home</Text>
+        </View>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>Perfil</Text>
+        </View>
+      </View>
+
+      <View style={[styles.sectionContainer, { flex: 3 }]}>
+        <View style={styles.darkCard} />
+      </View>
+
+      <View
+        style={[
+          styles.sectionContainer,
+          { flex: 2, flexDirection: "row", gap: 15 },
+        ]}
+      >
+        <View style={[styles.darkCard, { flex: 1 }]} />
+        <View style={[styles.darkCard, { flex: 1 }]} />
+      </View>
+
+      <View style={[styles.sectionContainer, { flex: 1.5 }]}>
+        <View style={styles.darkCard} />
+      </View>
     </View>
   );
 }
@@ -12,14 +37,37 @@ export default function Exercicio01() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#F5F5F7",
-    justifyContent: "center",
+    backgroundColor: "#b5b9d7",
+    padding: 20,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "#1e1e2c",
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    borderRadius: 12,
+    flex: 0.48,
     alignItems: "center",
   },
-  indicatorText: {
-    fontSize: 12,
-    color: "#8E8E93",
-    position: "absolute",
-    top: 50,
+  buttonText: {
+    color: "#ffffff",
+    fontSize: 16,
+    fontWeight: "500",
+  },
+  sectionContainer: {
+    borderWidth: 2,
+    borderColor: "#9EA4C9",
+    borderRadius: 20,
+    padding: 15,
+    marginBottom: 15,
+  },
+  darkCard: {
+    flex: 1,
+    backgroundColor: "#1E1E2C",
+    borderRadius: 12,
   },
 });
