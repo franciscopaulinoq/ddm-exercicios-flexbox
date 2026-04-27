f1afc325032e1185466a12cdd59ab1ea5f7cc742
Francisco Paulino de Queiroz <franciscopaulino.q@gmail.com>
2026-04-27 16:58:55 -0300

    feat: implement exercicio 4 screen

--- src/screens/Exercicio04.tsx
+++ src/screens/Exercicio04.tsx
@@ -1,10 +1,29 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-export default function Exercicio01() {
+export default function Exercicio04() {
   return (
     <View style={styles.container}>
-      <Text style={styles.indicatorText}>Exercício 04 - DDM</Text>
+      <View style={[styles.sectionContainer, { flex: 0.8 }]}>
+        <View style={styles.darkBar} />
+      </View>
+
+      <View style={[styles.sectionContainer, { flex: 4 }]}>
+        <View style={styles.darkCard} />
+      </View>
+
+      <View style={styles.rowContainer}>
+        <View style={[styles.sectionContainer, styles.halfColumn]}>
+          <View style={styles.darkCard} />
+        </View>
+        <View style={[styles.sectionContainer, styles.halfColumn]}>
+          <View style={styles.darkCard} />
+        </View>
+      </View>
+
+      <View style={[styles.sectionContainer, { flex: 0.8 }]}>
+        <View style={styles.darkBar} />
+      </View>
     </View>
   );
 }
@@ -12,14 +31,36 @@ export default function Exercicio01() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#F5F5F7",
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: "#b5b9d7",
+    padding: 20,
+    gap: 15,
   },
-  indicatorText: {
-    fontSize: 12,
-    color: "#8E8E93",
-    position: "absolute",
-    top: 50,
+  sectionContainer: {
+    borderWidth: 2,
+    borderColor: "#9ea4c9",
+    borderRadius: 20,
+    padding: 15,
+    justifyContent: "center",
+    backgroundColor: "rgba(255, 255, 255, 0.1)",
+  },
+  rowContainer: {
+    flexDirection: "row",
+    flex: 2,
+    gap: 15,
+  },
+  halfColumn: {
+    flex: 1,
+    marginBottom: 0,
+  },
+  darkCard: {
+    flex: 1,
+    backgroundColor: "#1e1e2c",
+    borderRadius: 12,
+  },
+  darkBar: {
+    height: 30,
+    backgroundColor: "#1e1e2c",
+    borderRadius: 8,
+    width: "100%",
   },
 });
